adbc4c197d13abac2ac5574be6a5f854bcf150fa
Nick Thompson <4nt@ornl.gov>
2021-03-02 11:10:06 -0500

    Tensor works now.

--- include/drt/tensor.hpp
+++ include/drt/tensor.hpp
@@ -13,30 +13,44 @@ public:
 
     tensor() {};
 
-    Real operator()(int64_t i, int64_t j, int64_t k) const {
-        return T_[i*panels + j*cols + k];
+    inline Real operator()(int64_t i, int64_t j, int64_t k) const {
+        return T_[i*rows*cols + j*cols + k];
     }
 
-    Real& operator()(int64_t i, int64_t j, int64_t k) {
-        return T_[i*panels + j*cols + k];
+    inline Real& operator()(int64_t i, int64_t j, int64_t k) {
+        return T_[i*rows*cols + j*cols + k];
     }
 
-    // Tensor contraction. T is a bilinear map from ℝⁿxℝⁿ -> ℝⁿ so that T(x,y) \in ℝⁿ
-    vec<Real, panels> operator()(vec<Real, rows> const & x, vec<Real, cols> const & y) const {
-
+    // Tensor contraction. T is a bilinear map from ℝⁿxℝⁿ -> ℝⁿ so that T(x,y) ∈ ℝⁿ.
+    vec<Real, panels> operator()(vec<Real, cols> const & x, vec<Real, rows> const & y) const {
+        vec<Real, panels> z;
+        for (int64_t i = 0; i < panels; ++i) {
+            z[i] = 0;
+            for (int64_t j = 0; j < cols; ++j) {
+                for (int64_t k = 0; k < rows; ++k) {
+                    z[i] += T_[i*rows*cols + j*cols + k]*x[j]*y[k];
+                }
+            }
+        }
+        return z;
     }
 
     friend std::ostream& operator<<(std::ostream & os, const tensor<Real, panels, rows, cols> & v)
     {
-        // This is totally wrong.
         for (int64_t i = 0; i < panels; ++i) {
-            for (int64_t j = 0; j < cols; ++j) {
+            os << "{";
+            for (int64_t j = 0; j < rows; ++j) {
+                if (j != 0) { os << " "; }
                 os << "[";
-                for (int64_t k = 0; k < panels; ++k) {
-                    os << v.T_[i*panels + j*cols] << ", ";
+                for (int64_t k = 0; k < cols - 1; ++k) {
+                    os << v.T_[i*rows*cols + j*cols + k] << ", ";
                 }
-                os << v.T_[i*panels + cols] << "]\n";
+                os << v.T_[i*rows*cols + j*cols + cols - 1] << "]";
+                if (j != rows - 1) {
+                    os << "\n";
+                }
             }
+            os << "}\n";
         }
         return os;
     }
--- tests/tensor_test.hpp
+++ tests/tensor_test.hpp
@@ -1,26 +1,140 @@
 #ifndef TENSOR_TEST_HPP
 #define TENSOR_TEST_HPP
+#include <random>
 #include <drt/tensor.hpp>
 
 using namespace drt;
 
-TEST(TensorTest, TwoByTwo) {
+TEST(TensorTest, 2x2x2) {
     using Real = double;
-    tensor<Real, 2,2,2> t;
-    for (int64_t i = 0; i < 2; ++i) {
-        for (int64_t j = 0; j < 2; ++j) {
-            for (int64_t k = 0; k < 2; ++k) {
-                t(i,j,k) = i*i + j*j + k*k;
+    constexpr const int64_t d = 2;
+    tensor<Real,d,d,d> T;
+    for (int64_t i = 0; i < d; ++i) {
+        for (int64_t j = 0; j < d; ++j) {
+            for (int64_t k = 0; k < d; ++k) {
+                if (i == j && j == k) {
+                    T(i,j,k) = 1;
+                } else {
+                    T(i,j,k) = 0;
+                }
             }
         }
     }
-    std::cout << t << "\n";
+
+    std::mt19937_64 gen(12345);
+    std::uniform_real_distribution<Real> dis(-1,1);
+    vec<Real, d> x;
+    vec<Real, d> y;
+    for (int64_t i = 0; i < d; ++i) {
+        x[i] = dis(gen);
+        y[i] = dis(gen);
+    }
+
+    auto z = T(x,y);
+
+    EXPECT_FLOAT_EQ(z[0], x[0]*y[0]);
+    EXPECT_FLOAT_EQ(z[1], x[1]*y[1]);
+
+    for (int64_t i = 0; i < d; ++i) {
+        for (int64_t j = 0; j < d; ++j) {
+            for (int64_t k = 0; k < d; ++k) {
+                T(i,j,k) = dis(gen);
+            }
+        }
+    }
+    vec<Real, d> zero;
+    zero[0] = 0;
+    zero[1] = 0;
+    z = T(x, zero);
+
+    EXPECT_FLOAT_EQ(z[0], 0);
+    EXPECT_FLOAT_EQ(z[1], 0);
+
+    // Bilinearity:
+    Real k1 = dis(gen);
+    Real k2 = dis(gen);
+    auto z1 = T(k1*x, k2*y);
+    z = k1*k2*T(x,y);
+    EXPECT_FLOAT_EQ(z[0], z1[0]);
+    EXPECT_FLOAT_EQ(z[1], z1[1]);
+
+    vec<Real,d> w;
+    w[0] = dis(gen);
+    w[1] = dis(gen);
+    z = T(x, k1*y + k2*w);
+    z1 = k1*T(x, y) + k2*T(x,w);
+
+    EXPECT_FLOAT_EQ(z[0], z1[0]);
+    EXPECT_FLOAT_EQ(z[1], z1[1]);
 }
 
 
-TEST(TensorTest, ThreeByThree) {
+TEST(TensorTest, 3x3x3) {
     using Real = double;
-    tensor<Real, 3,3,3> t;
-    
+    constexpr const int64_t d = 3;
+    tensor<Real,d,d,d> T;
+    for (int64_t i = 0; i < d; ++i) {
+        for (int64_t j = 0; j < d; ++j) {
+            for (int64_t k = 0; k < d; ++k) {
+                if (i == j && j == k) {
+                    T(i,j,k) = 1;
+                } else {
+                    T(i,j,k) = 0;
+                }
+            }
+        }
+    }
+
+    std::mt19937_64 gen(12345);
+    std::uniform_real_distribution<Real> dis(-1,1);
+    vec<Real, d> x;
+    vec<Real, d> y;
+    for (int64_t i = 0; i < d; ++i) {
+        x[i] = dis(gen);
+        y[i] = dis(gen);
+    }
+
+    auto z = T(x,y);
+
+    EXPECT_FLOAT_EQ(z[0], x[0]*y[0]);
+    EXPECT_FLOAT_EQ(z[1], x[1]*y[1]);
+    EXPECT_FLOAT_EQ(z[2], x[2]*y[2]);
+
+    for (int64_t i = 0; i < d; ++i) {
+        for (int64_t j = 0; j < d; ++j) {
+            for (int64_t k = 0; k < d; ++k) {
+                T(i,j,k) = dis(gen);
+            }
+        }
+    }
+    vec<Real, d> zero;
+    zero[0] = 0;
+    zero[1] = 0;
+    zero[2] = 0;
+    z = T(x, zero);
+
+    EXPECT_FLOAT_EQ(z[0], 0);
+    EXPECT_FLOAT_EQ(z[1], 0);
+    EXPECT_FLOAT_EQ(z[2], 0);
+
+    // Bilinearity:
+    Real k1 = dis(gen);
+    Real k2 = dis(gen);
+    auto z1 = T(k1*x, k2*y);
+    z = k1*k2*T(x,y);
+    EXPECT_FLOAT_EQ(z[0], z1[0]);
+    EXPECT_FLOAT_EQ(z[1], z1[1]);
+    EXPECT_FLOAT_EQ(z[2], z1[2]);
+
+    vec<Real,d> w;
+    w[0] = dis(gen);
+    w[1] = dis(gen);
+    w[2] = dis(gen);
+    z = T(x, k1*y + k2*w);
+    z1 = k1*T(x, y) + k2*T(x,w);
+
+    EXPECT_FLOAT_EQ(z[0], z1[0]);
+    EXPECT_FLOAT_EQ(z[1], z1[1]);
+    EXPECT_FLOAT_EQ(z[2], z1[2]);
 }
 #endif
